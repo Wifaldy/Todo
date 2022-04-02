6b0c9cae9d75bf9b565f8bd1aebb13567866271d
Andri Wifaldy <andriwifaldy@gmail.com>
2022-04-02 09:38:28 +0700

    Connect User and Todo

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,8 +1,12 @@
 const Todo = require("../models/todoModel");
 
-exports.getTodos = async(req, res) => {
+exports.getTodos = async(req, res, next) => {
     try {
-        const todos = await Todo.findAll();
+        const todos = await Todo.findAll({
+            where: {
+                userId: req.userId
+            }
+        });
         res.status(200).json({
             message: "Todos fetched successfully",
             data: todos
@@ -11,12 +15,13 @@ exports.getTodos = async(req, res) => {
         console.log(err);
     }
 }
-exports.postAddTodo = async(req, res) => {
+exports.postAddTodo = async(req, res, next) => {
     const title = req.body.title;
     try {
         const todo = await Todo.create({
             title: title,
-            completed: false
+            completed: false,
+            userId: req.userId
         });
         res.status(200).json({
             message: "Todo added successfully",
@@ -27,33 +32,61 @@ exports.postAddTodo = async(req, res) => {
     }
 }
 
-exports.updateTodo = async(req, res) => {
+exports.updateTodo = async(req, res, next) => {
     const id = req.params.id;
     const title = req.body.title;
+    const completed = req.body.completed == "true" || req.body.completed == true;
     try {
         const todo = await Todo.findByPk(id);
+        if (!todo) {
+            const error = new Error("Todo not found");
+            error.statusCode = 404;
+            throw error;
+        }
+        if (todo.userId !== req.userId) {
+            const error = new Error("Not authorized");
+            error.statusCode = 403;
+            throw error;
+        }
         todo.title = title;
         todo.updatedAt = new Date();
+        todo.completed = completed;
         await todo.save();
         res.status(200).json({
             message: "Todo updated successfully",
             data: todo
         })
     } catch (err) {
-        console.log(err);
+        if (!err.statusCode) {
+            err.statusCode = 500;
+        }
+        next(err);
     }
 }
 
-exports.deleteTodo = async(req, res) => {
+exports.deleteTodo = async(req, res, next) => {
     const id = req.params.id;
     try {
         const todo = await Todo.findByPk(id);
+        if (!todo) {
+            const error = new Error("Todo not found");
+            error.statusCode = 404;
+            throw error;
+        }
+        if (todo.userId !== req.userId) {
+            const error = new Error("Not authorized");
+            error.statusCode = 403;
+            throw error;
+        }
         await todo.destroy();
         res.status(200).json({
             message: "Todo deleted successfully",
             data: todo
         })
     } catch (err) {
-        console.log(err);
+        if (!err.statusCode) {
+            err.statusCode = 500;
+        }
+        next(err);
     }
 }
